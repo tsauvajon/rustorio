abf47c50680ff04be7f75675875e7b692bb50bd8
Thomas Sauvajon <thomas.sauvajon@proton.me>
2026-02-01 16:11:35 +0100

    Build an assembler

--- src/bin/standard/main.rs
+++ src/bin/standard/main.rs
@@ -1,11 +1,15 @@
 #![forbid(unsafe_code)]
 
+use std::ops::{Add, AddAssign};
+
 use rustorio::{
-    self, Bundle, InsufficientResourceError, Resource, ResourceType, Tick,
-    buildings::Furnace,
+    self, Bundle, HandRecipe, InsufficientResourceError, Recipe, Resource, ResourceType, Tick,
+    buildings::{Assembler, Furnace},
     gamemodes::Standard,
-    recipes::{CopperSmelting, FurnaceRecipe, IronSmelting},
-    resources::{Copper, CopperOre, Iron, IronOre, Point},
+    recipes::{
+        CopperSmelting, CopperWireRecipe, ElectronicCircuitRecipe, FurnaceRecipe, IronSmelting,
+    },
+    resources::{Copper, CopperOre, CopperWire, ElectronicCircuit, Iron, IronOre, Point},
     territory::{Miner, Territory},
 };
 
@@ -28,6 +32,7 @@ fn user_main(mut tick: Tick, starting_resources: StartingResources) -> (Tick, Bu
     let iron_furnace = Furnace::build(&tick, IronSmelting, iron);
     assert_eq!(0, iron.amount());
 
+    // Miner
     let iron = SmeltIron {}
         .mine_and_smelt::<10>(&mut tick, iron_territory, iron_furnace)
         .unwrap();
@@ -38,11 +43,36 @@ fn user_main(mut tick: Tick, starting_resources: StartingResources) -> (Tick, Bu
     let miner = Miner::build(iron, copper);
     copper_territory.add_miner(&tick, miner);
 
-    // We have 2 furnaces and a miner
+    // Assembler
+    let iron_furnace = copper_furnace.change_recipe(IronSmelting).unwrap();
+    let iron = SmeltIron {}
+        .mine_and_smelt::<6>(&mut tick, iron_territory, iron_furnace)
+        .unwrap();
 
     // Costs 12 [copper wires](crate::resources::CopperWire) and 6 [iron](crate::resources::Iron).
-    // So we need 24 copper
-    // Assembler::build(tick, recipe, copper_wires, iron)
+    // So we need 6 copper
+    let copper = SmeltCopper {}
+        .mine_and_smelt(&mut tick, copper_territory, copper_furnace)
+        .unwrap();
+    let mut copper_wires = CopperWireRecipe::craft(&mut tick, (copper,))
+        .0
+        .to_resource();
+    for _ in 0..5 {
+        let copper = SmeltCopper {}
+            .mine_and_smelt(&mut tick, copper_territory, copper_furnace)
+            .unwrap();
+        let new_copper_wires = CopperWireRecipe::craft(&mut tick, (copper,))
+            .0
+            .to_resource();
+        copper_wires.add_assign(new_copper_wires);
+    }
+
+    let assembler = Assembler::build(
+        &tick,
+        ElectronicCircuitRecipe,
+        copper_wires.bundle().unwrap(),
+        iron,
+    );
 }
 
 trait Smelting {
@@ -64,19 +94,29 @@ trait Smelting {
     fn mine_and_smelt<const AMOUNT: u32>(
         &self,
         tick: &mut Tick,
-        mut iron_territory: Territory<Self::Ore>,
+        iron_territory: Territory<Self::Ore>,
         mut furnace: Furnace<Self::Recipe>,
     ) -> Result<Bundle<Self::Smelted, AMOUNT>, InsufficientResourceError<Self::Smelted>> {
-        for _ in 0..AMOUNT {
-            let iron_ore = iron_territory.hand_mine::<1>(tick);
-            self.first_input(tick, &mut furnace).add_bundle(iron_ore);
-        }
+        self.hand_mine_into_furnace::<AMOUNT>(tick, iron_territory, &mut furnace);
 
+        // optimise: only ever tick by hand mining, never by just waiting
         tick.advance_until(
             |tick| self.first_output(tick, &mut furnace).amount().ge(&AMOUNT),
             u64::MAX,
         );
         self.first_output(tick, &mut furnace).bundle()
+    }
+
+    fn hand_mine_into_furnace<const AMOUNT: u32>(
+        &self,
+        tick: &mut Tick,
+        mut iron_territory: Territory<Self::Ore>,
+        mut furnace: &mut Furnace<Self::Recipe>,
+    ) {
+        for _ in 0..AMOUNT {
+            let iron_ore = iron_territory.hand_mine::<1>(tick);
+            self.first_input(tick, &mut furnace).add_bundle(iron_ore);
+        }
     }
 }
 
